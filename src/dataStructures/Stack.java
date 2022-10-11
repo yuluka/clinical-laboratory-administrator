72484358c0c4f462a488e0898489510960136ed6
Yuluka Gigante Muriel <yulukeitor@gmail.com>
2022-10-11 10:47:40 -0500

    Queue and stack tests.
    The JUnit tests for the queues and stacks has been implemented.

--- src/dataStructures/Stack.java
+++ src/dataStructures/Stack.java
@@ -3,9 +3,10 @@ package dataStructures;
 public class Stack<T> {
 	
 	private StackNode<T> top;
+	private int size;
 
 	public Stack() {
-		
+		this.size = 0;
 	}
 	
 	public void push(StackNode<T> newNode) {
@@ -15,25 +16,35 @@ public class Stack<T> {
 			newNode.setNext(top);
 			top = newNode;
 		}
+		
+		++size;
+	}
+	
+	public void push(T newNodeValue) {
+		StackNode<T> newNode = new StackNode<>(newNodeValue);
+		
+		push(newNode);
 	}
 	
 	public StackNode<T> pop() {
 		StackNode<T> node = null;
 		
 		if(top == null) {
-			//Lanzar excepción
 			System.out.println("Conjunto vacío");
+			return null;
 		} else {
 			node = top;
 			top = top.getNext();
 		}
+		
+		--size;
 		
 		return node;
 	}
 	
 	public StackNode<T> top() {
 		if(top == null) {
-			//Lanzar excepción
+			return null;
 		}
 		
 		return top;
@@ -45,5 +56,21 @@ public class Stack<T> {
 		} else {
 			return false;
 		}
+	}
+	
+	public boolean contains(T elementSearched) {
+		if(top == null) {
+			return false;
+		}
+		
+		StackNode<T> current = top;
+		
+		for (int i = 0; i < size; i++) {
+			if(current.getValue().equals(elementSearched)) {
+				return true;
+			}
+		}
+		
+		return false;
 	}
 }
